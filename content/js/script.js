ed67e2b93c9101e7fc50356560e3dd780761539a
Roni <roni691986@gmail.com>
2019-02-14 20:10:00 +0200

    finish - optimizes

--- content/js/script.js
+++ content/js/script.js
@@ -1,42 +1,38 @@
 $(function() {
-
     $(".secondSection .whiteBorder").animate({
-        'border-top-width':"100%",
-    },1000);
-
-    $('.sixthWrapper ul').slick({
+        "border-top-width": "100%"
+    }, 1e3);
+    $(".sixthWrapper ul").slick({
         dots: true,
-        nextArrow: $('#nextArrow'),
-        prevArrow: $('#prevArrow')
+        nextArrow: $("#nextArrow"),
+        prevArrow: $("#prevArrow")
     });
-
-    $('#search , .xBT , #overlay').click( function() {
-        console.log("x");
-        var searchOpen = $('#searchWrapper').height() > 0 ? false : true;
-        if (searchOpen){
-            $('#searchWrapper').animate({ height: "427px" });
+    $("#search , .xBT , #overlay").click(function() {
+        var a = $("#searchWrapper").height() > 0 ? false : true;
+        if (a) {
+            $("#searchWrapper").animate({
+                height: "427px"
+            });
             $("#overlay").fadeIn();
-        }
-        else{
-            $('#searchWrapper').animate({ height: "0px" });
+        } else {
+            $("#searchWrapper").animate({
+                height: "0px"
+            });
             $("#overlay").fadeOut();
         }
     });
-
-    $(".forthSection ul li").hover(function() { 
+    $(".forthSection ul li").hover(function() {
         $(this).find(".hImgTitle").hide();
         $(this).find(".Group-20-Copy-11").hide();
         $(this).find(".hoveredImage").show();
-    }, function(){  
+    }, function() {
         $(this).find(".hImgTitle").show();
         $(this).find(".Group-20-Copy-11").show();
         $(this).find(".hoveredImage").hide();
     });
-
-    $('#searchInput').focus( function() {
+    $("#searchInput").focus(function() {
         $(".searchResults").fadeIn();
     }).focusout(function() {
         $(".searchResults").fadeOut();
-      })
-    
+    });
 });
